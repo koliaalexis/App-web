1ab64dd59121eb67f9ee92a5ee0af694df1772a5
koliaalexis <alexis.thomas@eemi.com>
2024-09-26 15:25:02 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
 st.title("Dall-e 3")
+
 st.write ("Veuillez entré une description de l'image que vous souhaitez generer")
+
+dalle_input = st.text_input ("Tapez votre texte : ")
+st.write(dalle_input)

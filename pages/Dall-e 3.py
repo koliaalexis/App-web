5e06c7757d788652a54f4d9b31bacf94415e0a86
koliaalexis <alexis.thomas@eemi.com>
2024-09-26 15:57:02 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -3,10 +3,10 @@ import streamlit as st
 st.title("Dall-e 3")
 
 #Champ de saisie
-texte_input = st.text_input("Application Web - Open IA")
-st.write(texte_input)
+user_input = st.text_input("Application Web - Open IA")
+st.write(user_input)
 
-#Champ de saisie dans la sidebar
+#Champ de saisie dans la sidebar (pour la clé OpenAI)
 sidebar_input = st.sidebar.text_input("Tapez la clé OpenAI ici :")
 st.write(sidebar_input)
 
@@ -14,11 +14,11 @@ st.write(sidebar_input)
 from openai import OpenAI
 client = OpenAI(api_key=sidebar_input)
 
-prompt = "A boat"
+prompt = "A cute baby sea otter"
 
 image = client.images.generate(
     model="dall-e-2",
-    prompt=texte_input,
+    prompt=user_input,
     size="512x512",
     quality="standard",
     n=1,

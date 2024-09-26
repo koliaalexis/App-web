9da8ba28cbc70332c17e9a9eec2f726e438ce62c
koliaalexis <alexis.thomas@eemi.com>
2024-09-26 15:51:00 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -4,7 +4,25 @@ st.title("Dall-e 3")
 
 #st.write ("Veuillez entré une description de l'image que vous souhaitez generer")
 
-dalle_input = st.text_input ("Veuillez entré une description de l'image que vous souhaitez generer")
-st.write(dalle_input)
+text_input = st.text_input("Application Web - Open IA")
+st.write(text_input)
 
-st.sidebar.text_input("Veuillez entré la clé Open IA")
+sidebar_in = st.sidebar.text_input("Veuillez entré la clé Open IA")
+
+#Intéraction avec OpenAI
+from openai import OpenAI
+client = OpenAI(api_key=sidebar_in)
+
+prompt = "A boat"
+
+image = client.images.generate(
+    model="dall-e-2",
+    prompt=text_input,
+    size="512x512",
+    quality="standard",
+    n=1,
+)
+image_url = image.data[0].url
+
+#Affichage de l'image
+st.image(image_url)

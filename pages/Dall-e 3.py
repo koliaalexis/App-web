49fb0620400b3068c17f86a5edc2b3fb18e9ea4e
koliaalexis <alexis.thomas@eemi.com>
2024-09-26 15:25:43 +0200

    Update Dall-e 3.py

--- pages/Dall-e 3.py
+++ pages/Dall-e 3.py
@@ -4,5 +4,5 @@ st.title("Dall-e 3")
 
 st.write ("Veuillez entré une description de l'image que vous souhaitez generer")
 
-dalle_input = st.text_input ("Tapez votre texte : ")
+dalle_input = st.text_input ("")
 st.write(dalle_input)
